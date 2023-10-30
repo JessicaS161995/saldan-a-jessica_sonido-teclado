9441ed2fd25381ac65e801ba56d8ccdcbd659b88
JessicaS161995 <ojitos16@outlook.es>
2023-10-30 16:55:46 -0500

    se puso sonidos el goce

--- sketch.js
+++ sketch.js
@@ -1,24 +1,109 @@
+//sonidomar.play();
+  //soundFormats('wav', 'mp3', 'ogg');
+//sonidomar = loadSound('sonido/mar');
+  
+//function mouseDragged() {
+  //sonidomar.play();
+ // circle(mouseX, mouseY, 20);
+
+
+  
 let sonidomar;
+let sonidos = [];
 
 function preload() {
-  soundFormats('wav', 'mp3', 'ogg');
-  sonidomar = loadSound('sonido/mar');
+
+  for (let i = 0; i < 7; i++) {
+		sonidos[i] = loadSound('sonido/' + i + '.mp3');
+	}
+ 
 }
 function setup() {
   let cvn = createCanvas(windowWidth, windowheight);
-  cvn.mousePressed(presionar);
+  cnv.mouseClicked(presionado);
 }
 
 function presionar() {
-  sonidomar.play();
-  circle(mouseX, mouseY, 20);
+  let r = Math.floor(random(7));
+	circle(mouseX, mouseY, 20);
+
+	switch (r) {
+		case 0:
+			sonidos[0].play();
+			break;
+		case 1:
+			sonidos[1].play();
+			break;
+		case 2:
+			sonidos[2].play();
+			break;
+		case 3:
+			sonidos[3].play();
+			break;
+		case 4:
+			sonidos[4].play();
+			break;
+		case 5:
+			sonidos[5].play();
+			break;
+		case 6:
+			sonidos[6].play();
+			break;
+	}
 }
   
-function draw() {
-  
+function draw() {}
+function mouseDragged() {
+	let r = Math.floor(random(7));
+	circle(mouseX, mouseY, 20);
+
+	switch (r) {
+		case 0:
+			sonidos[0].play();
+			break;
+		case 1:
+			sonidos[1].play();
+			break;
+		case 2:
+			sonidos[2].play();
+			break;
+		case 3:
+			sonidos[3].play();
+			break;
+		case 4:
+			sonidos[4].play();
+			break;
+		case 5:
+			sonidos[5].play();
+			break;
+		case 6:
+			sonidos[6].play();
+			break;
+	}
 }
 
-function mouseDragged() {
-  sonidomar.play();
-  circle(mouseX, mouseY, 20);
+function keyPressed() {
+	switch (key) {
+		case 'a':
+			sonidos[0].play();
+			break;
+		case 's':
+			sonidos[1].play();
+			break;
+		case 'd':
+			sonidos[2].play();
+			break;
+		case 'f':
+			sonidos[3].play();
+			break;
+		case 'g':
+			sonidos[4].play();
+			break;
+		case 'h':
+			sonidos[5].play();
+			break;
+		case 'j':
+			sonidos[6].play();
+			break;
+	}
 }
